6ce0e45062958f96ecff9b671d67ae706207575f
hansoo kim <bughunt88@gmail.com>
2021-06-16 15:06:17 +0900

    AI_Team_Project_Flask

--- flask_team.py
+++ flask_team.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template , request
 app = Flask(__name__)
  
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+def home():
+    return render_template("map.html")
+
+
+@app.route('/view')
+
+def view():
+    name = request.args.get("name")
+
+    print(name)
+
+    return render_template("table.html", subject = name)
  
 if __name__ == '__main__':
+    
     app.run()
