53db5451a19c4d248dfb0959614119e9b04d04a9
hansoo kim <bughunt88@gmail.com>
2021-07-02 14:51:54 +0900

    AI_Team_Project

--- flask_team.py
+++ flask_team.py
@@ -30,70 +30,70 @@ def view():
     name = request.args.get("name")
     cate = str(request.args.get("cate"))
 
-    
-    #x_pred = load_data("SELECT d.date,YEAR,MONTH, d.day, d.time, category, dong,temperature,rain,wind,humidity, IFNULL( c_person,0) AS person, VALUE FROM main_data_table AS d INNER JOIN `weather` AS s ON d.date = s.DATE AND d.time = s.time LEFT JOIN `covid19_re` AS c ON c.date = d.date ", is_train = False)
-    #x_train, y_train = load_data("SELECT d.date,YEAR,MONTH, d.day, d.time, category, dong,temperature,rain,wind,humidity, IFNULL( c_person,0) AS person, VALUE FROM main_data_table AS d INNER JOIN `weather` AS s ON d.date = s.DATE AND d.time = s.time LEFT JOIN `covid19_re` AS c ON c.date = d.date WHERE (d.TIME != 2 AND d.TIME != 3 AND d.TIME != 4 AND d.TIME != 5  AND d.TIME != 6 AND d.TIME != 7 AND d.TIME != 8) ORDER BY DATE, YEAR, MONTH, DAY, TIME, category, dong ASC")
-
-    location = " and a.dong ='"
-    
-    if name =='구로구':
-        location += "0"
-    elif name =='영등포구':
-        location += "1"
-    elif name =='도봉구':
-        location += "2"
-    elif name =='성북구':
-        location += "3"
-    elif name =='동작구':
-        location += "4"
-    elif name =='서대문구':
-        location += "5"
-    elif name =='노원구':
-        location += "6"
-    elif name =='은평구':
-        location += "7"
-    elif name =='마포구':
-        location += "8"
-    elif name =='금천구':
-        location += "9"
-    elif name =='강서구':
-        location += "10"
-    elif name =='양천구':
-        location += "11"
-    elif name =='강북구':
-        location += "12"
-    elif name =='관악구':
-        location += "13"
-    elif name =='송파구':
-        location += "14"
-    elif name =='강남구':
-        location += "15"
-    elif name =='서초구':
-        location += "16"
-    elif name =='용산구':
-        location += "17"
-    elif name =='동대문구':
-        location += "18"
-    elif name =='강동구':
-        location += "19"
-    elif name =='중랑구':
-        location += "20"
-    elif name =='중구':
-        location += "21"
-    else:
-        location += "22"
-
-    location += "'"
-
-    cate_list = ""
+    x_pred = []
 
     if cate != "None":
-        cate_list += " and category = '" + cate + "' "
+
+        location = " and a.dong ='"
+        
+        if name =='구로구':
+            location += "0"
+        elif name =='영등포구':
+            location += "1"
+        elif name =='도봉구':
+            location += "2"
+        elif name =='성북구':
+            location += "3"
+        elif name =='동작구':
+            location += "4"
+        elif name =='서대문구':
+            location += "5"
+        elif name =='노원구':
+            location += "6"
+        elif name =='은평구':
+            location += "7"
+        elif name =='마포구':
+            location += "8"
+        elif name =='금천구':
+            location += "9"
+        elif name =='강서구':
+            location += "10"
+        elif name =='양천구':
+            location += "11"
+        elif name =='강북구':
+            location += "12"
+        elif name =='관악구':
+            location += "13"
+        elif name =='송파구':
+            location += "14"
+        elif name =='강남구':
+            location += "15"
+        elif name =='서초구':
+            location += "16"
+        elif name =='용산구':
+            location += "17"
+        elif name =='동대문구':
+            location += "18"
+        elif name =='강동구':
+            location += "19"
+        elif name =='중랑구':
+            location += "20"
+        elif name =='중구':
+            location += "21"
+        else:
+            location += "22"
+
+        location += "'"
+
+        cate_list = ""
+
+        if cate != "None":
+            cate_list += " and a.category = '" + cate + "' "
 
 
-    sql = "SELECT a.DATE,a.YEAR,a.MONTH,a.DAY,a.TIME,a.category,a.dong,a.VALUE FROM result_data_table AS a LEFT JOIN main_data_table AS s ON s.date = DATE_SUB(a.date, INTERVAL 1 YEAR) AND a.month=s.month AND a.time=s.time AND a.category=s.category AND a.dong = s.dong  where 1"+location + cate_list
-    sql += " order by a.date, a.year, a.month, a.day, a.time asc"
-    x_pred = load_data(sql)
+        sql = "SELECT a.DATE,a.YEAR,a.MONTH,a.DAY,a.TIME,a.category,a.dong,a.VALUE FROM result_data_table AS a LEFT JOIN main_data_table AS s ON s.date = DATE_SUB(a.date, INTERVAL 1 YEAR) AND a.month=s.month AND a.time=s.time AND a.category=s.category AND a.dong = s.dong  where 1"+location + cate_list
+        sql += " order by a.date, a.year, a.month, a.day, a.time asc"
+        x_pred = load_data(sql)
 
     return render_template("table.html", subject = name, data = x_pred, cate = cate)
  
